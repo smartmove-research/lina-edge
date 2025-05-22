a4f1f4e64a8bd3f9532831fdb9dc46a20a5dc24e
AndreKev <ak650283114@gmail.com>
2025-05-22 07:21:22 +0100

    log bluetooth device profile setup to console

--- setup/ensure_bluetooth_mic.sh
+++ setup/ensure_bluetooth_mic.sh
@@ -1,2 +1,5 @@
+echo "Setting Up device bluez_card.3C_7A_C0_A5_CE_39 to handsfree_head_unit..."
 pactl set-card-profile bluez_card.3C_7A_C0_A5_CE_39 handsfree_head_unit
+
+echo "Setting Up device bluez_card.41_42_FF_20_59_A3 to handsfree_head_unit..."
 pactl set-card-profile bluez_card.41_42_FF_20_59_A3 handsfree_head_unit
